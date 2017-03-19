08d5f341233e10c94e6da03982994f227e7ecfb5
vitality <vitaly.kiktiov@gmail.com>
2017-03-19 13:52:23 +0200

    Calculate and display todays solar data

--- app/containers/TodayScene.js
+++ app/containers/TodayScene.js
@@ -1,24 +1,48 @@
 /*
- @flow
+
 
  */
 
 import React, {Component, PropTypes} from 'react';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
-import {Container, Header, Body, Title, Content, Text, Grid, Row, Card} from 'native-base';
+import {Container, Header, Body, Title, Content, Text, List, ListItem} from 'native-base';
 import moment from 'moment';
+import suncalc from 'suncalc';
 
 import * as todayActions from '../actions/todayActions';
 
 
 class TodayScene extends Component {
 
+  state = {
+    now: moment()
+  };
+  _ticker = 0;
+
+  tick = () => {
+    const { position: { coords: { latitude = null, longitude = null } } } = this.props;
+    const now = moment();
+
+    if (!latitude || !longitude) {
+      return this.setState({
+        now
+      })
+    }
+
+    const sunTimes = suncalc.getTimes(now, latitude, longitude);
+
+    this.setState({
+      now,
+      sunTimes
+    })
+  };
+
   componentDidMount() {
 
     const { actions } = this.props;
 
-    actions.setDate(moment());
+    this._ticker = setInterval(this.tick, 1000);
     actions.startPositionFetching();
 
     navigator.geolocation.getCurrentPosition(
@@ -30,8 +54,13 @@ class TodayScene extends Component {
     );
   }
 
+  componentWillUnmount = () => {
+    clearInterval(this._ticker);
+  }
+
   render() {
-    const { position: { coords: { latitude = null, longitude = null } }, date = moment() } = this.props;
+    const { position: { coords: { latitude = null, longitude = null } } } = this.props;
+    const { now } = this.state;
     return (
       <Container>
         <Header>
@@ -40,17 +69,30 @@ class TodayScene extends Component {
           </Body>
         </Header>
         <Content>
-          <Text>{ date.toString() }</Text>
+          <Text>{ now.toString() }</Text>
           <Text>Latitude: { latitude }, Longitude: { longitude }</Text>
+          {this._renderSunTimes()}
         </Content>
       </Container>
     );
+  }
+
+  _renderSunTimes = () => {
+    const { sunTimes } = this.state;
+    if (!sunTimes) return null;
+
+    const sunTimesTitles = Object.keys(sunTimes);
+
+    const sunTimesListItems = sunTimesTitles.map(suntime => {
+      return <ListItem key={suntime}><Text>{suntime}: {sunTimes[suntime].toString()}</Text></ListItem>
+    });
+
+    return <List>{sunTimesListItems}</List>;
   }
 }
 
 export default connect(state => ({
     position: state.today.position,
-    date: state.today.date,
     inProgress: state.today.fetching
   }),
   (dispatch) => ({
